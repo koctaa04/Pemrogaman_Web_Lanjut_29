3b515eade573afe52e4c6f70b37926b6014bdf78
koctaa04 <yeftaocta04@gmail.com>
2025-03-12 20:53:41 +0700

    Praktikum 2.2 : Not Found Exceptions

--- Pertemuan 4/PWL-POS/app/Http/Controllers/UserController.php
+++ Pertemuan 4/PWL-POS/app/Http/Controllers/UserController.php
@@ -9,16 +9,20 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-
+        // Praktikum 2.1
         // $user = UserModel::find(1);
         // $user = UserModel::where('level_id', 1)->first();
         // $user = UserModel::firstwhere('level_id', 1);
         // $user = UserModel::findOr(1,['username', 'nama'], function(){
         //     abort(404);
         // });
-        $user = UserModel::findOr(20,['username', 'nama'], function(){
-            abort(404);
-        });
+        // $user = UserModel::findOr(20,['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        
+        // Praktikum 2.2
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username','manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
