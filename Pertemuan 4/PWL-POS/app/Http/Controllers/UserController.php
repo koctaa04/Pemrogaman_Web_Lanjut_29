e56c46c5d29c56314ae227684ac8551224434a15
koctaa04 <yeftaocta04@gmail.com>
2025-03-12 23:11:28 +0700

    Praktikum 2.5 – Attribute Changes

--- Pertemuan 4/PWL-POS/app/Http/Controllers/UserController.php
+++ Pertemuan 4/PWL-POS/app/Http/Controllers/UserController.php
@@ -51,16 +51,59 @@ class UserController extends Controller
         //         'nama' => 'Manager',
         //     ],
         // );
-        $user = UserModel::firstOrNew(
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->save();
+
+        // Pertemuan 2.5
+        // $user = UserModel::create(
+        //     [
+        //         'username' => 'manager5',
+        //         'nama' => 'Manager Lima Lima',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->username = 'manager56';
+        // $user->isDirty(); // true
+        // $user->isDirty('username'); // true
+        // $user->isDirty('nama'); // false
+        // $user->isDirty('nama', 'username'); // true
+
+        // $user->isClean(); // false
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); // true
+        // $user->isClean('nama', 'username'); // false
+
+        // $user->save();
+
+        // $user->isDirty(); //false
+        // $user->isClean(); //true
+
+        // dd($user->isDirty());
+
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ],
         );
+
+        $user->username = 'manager12';
         $user->save();
-        return view('user', ['data' => $user]);
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['username', 'level_id']); //true
+        $user->wasChanged('nama'); //false
+        dd($user->wasChanged(['nama', 'username'])); //true
     }
 }
 
