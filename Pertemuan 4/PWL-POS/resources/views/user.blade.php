dc64df2c616e7f617509ec07eb03ebe942f9d4ad
koctaa04 <yeftaocta04@gmail.com>
2025-03-13 10:47:40 +0700

    Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

--- Pertemuan 4/PWL-POS/resources/views/user.blade.php
+++ Pertemuan 4/PWL-POS/resources/views/user.blade.php
@@ -1,43 +1,71 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Data User</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <title>Data User</title>
 
-    </head>
-    <body>
-        <h1>Data User</h1>
-        {{-- <p>Jumlah Pengguna : {{ $data }}</p> --}}
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <table border="1" cellpadding="2" cellspacing="0">
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th>
-            </tr>
-            <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
-            </tr>
-        </table>
-    </body>
-</html>
+</head>
 
+<body>
+    <h1>Data User</h1>
 
-            {{-- @foreach($data as $d)
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+            <th>Aksi</th>
+        </tr>
+        @foreach ($data as $d)
             <tr>
                 <td>{{ $d->user_id }}</td>
                 <td>{{ $d->username }}</td>
                 <td>{{ $d->nama }}</td>
                 <td>{{ $d->level_id }}</td>
+                <td>
+                    <a href="/user/ubah/{{ $d->user_id }}">Ubah</a> |
+                    <a href="/user/hapus/{{ $d->user_id }}">Hapus</a>
+                </td>
             </tr>
-            @endforeach --}}
+        @endforeach
+    </table>
+</body>
+
+</html>
+
+
+
+{{-- <h1>Data User</h1>
+    <p>Jumlah Pengguna : {{ $data }}</p>
+
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr>
+    </table> --}}
+
+{{-- @foreach ($data as $d)
+    <tr>
+        <td>{{ $d->user_id }}</td>
+        <td>{{ $d->username }}</td>
+        <td>{{ $d->nama }}</td>
+        <td>{{ $d->level_id }}</td>
+    </tr>
+@endforeach --}}
