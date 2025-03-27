99a6e6cbc6516642571b73a6b1fea42b534ac030
koctaa04 <yeftaocta04@gmail.com>
2025-03-27 19:30:21 +0700

    jobsheet 5 New - Praktikum 3 – Implementasi jQuery Datatable di AdminLTE

--- Pertemuan 5/PWL-POS-New/routes/web.php
+++ Pertemuan 5/PWL-POS-New/routes/web.php
@@ -21,14 +21,33 @@ use App\Http\Controllers\WelcomeController;
 //     return view('welcome');
 // });
 Route::get('/', [WelcomeController::class, 'index']);
-Route::get('/level', [LevelController::class, 'index']);
 
-Route::get('/kategori', [KategoriController::class, 'index']);
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);      // Menampilkan halaman awal user
+    Route::get('/list', [UserController::class, 'list']);   // Menampilkan daftar user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);   // Menampilkan halaman tambah user
+    Route::post('/', [UserController::class, 'store']);   // Menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);   // Menampilkan halaman detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);   // Menampilkan halaman ubah user
+    Route::put('/{id}', [UserController::class, 'update']);   // Mengubah data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);   // Menghapus data user
+});
 
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
-// Route::delete('/user/hapus/{id}',[UserController::class,'hapus']);
+
+
+
+
+
+
+
+// Route::get('/level', [LevelController::class, 'index']);
+
+// Route::get('/kategori', [KategoriController::class, 'index']);
+
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+// Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
+// // Route::delete('/user/hapus/{id}',[UserController::class,'hapus']);
