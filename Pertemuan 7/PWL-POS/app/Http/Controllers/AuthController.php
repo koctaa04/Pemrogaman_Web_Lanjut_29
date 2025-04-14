9aec66cc43fd3609c8b435b02b6e3e7c81bdd689
koctaa04 <yeftaocta04@gmail.com>
2025-04-14 14:40:07 +0700

    jobsheet 7 - Tugas 3 dan 4

--- Pertemuan 7/PWL-POS/app/Http/Controllers/AuthController.php
+++ Pertemuan 7/PWL-POS/app/Http/Controllers/AuthController.php
@@ -51,4 +51,48 @@ class AuthController extends Controller
 
         return redirect('login');
     }
+    public function register()
+    {
+        if (Auth::check()) {
+            return redirect('/');
+        }
+
+        $levels = LevelModel::all(); // Ambil semua level dari database
+        return view('auth.register', compact('levels'));
+    }
+
+    public function postRegister(Request $request)
+    {
+        $rules = [
+            'level_id' => 'required|exists:m_level,level_id',
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama' => 'required|string|max:100',
+            'password' => 'required|min:6|confirmed',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false, // response status, false: error/gagal, true: berhasil
+                'message' => 'Validasi Gagal',
+                'msgField' => $validator->errors() // pesan error validasi
+            ]);
+        }
+
+        $user = UserModel::create([
+            'level_id' => $request->level_id,
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Registrasi Berhasil',
+            'redirect' => url('/')
+        ]);
+    }
 }
