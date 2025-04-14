6273ea87725bf93eb5e8acb49baefc7678b9621f
koctaa04 <yeftaocta04@gmail.com>
2025-04-14 14:21:02 +0700

    jobsheet 7 - Tugas 2

--- Pertemuan 7/PWL-POS/app/Models/UserModel.php
+++ Pertemuan 7/PWL-POS/app/Models/UserModel.php
@@ -30,8 +30,26 @@ class UserModel extends Authenticatable
         'password' => 'hashed'
     ];
 
-    function level():BelongsTo
-    {   
+    function level(): BelongsTo
+    {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+
+    
+    // Mendapatkan nama role
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    // Cek apakah user memiliki role tertentu
+    public function hasRole(string $role): bool
+    {
+        return $this->level->level_kode === $role;
+    }
+
+    public function getRole() {
+        return $this->level->level_kode;
+    }
+
 }
