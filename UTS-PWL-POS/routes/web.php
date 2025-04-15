ced8ddf623e97e04bbbcf5bd35db98e7f122583b
koctaa04 <yeftaocta04@gmail.com>
2025-04-16 00:55:46 +0700

    feat: add index routes for all controllers

--- UTS-PWL-POS/routes/web.php
+++ UTS-PWL-POS/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StokController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\SupplierController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +22,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/barang', [BarangController::class, 'index'])->name('barang.index');
+Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
+Route::get('/level', [LevelController::class, 'index'])->name('level.index');
+Route::get('/stok', [StokController::class, 'index'])->name('stok.index');
+Route::get('/supplier', [SupplierController::class, 'index'])->name('supplier.index');
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
