cb430fcecbe3e45eedc15dfe95a9fe3d63ef287b
koctaa04 <yeftaocta04@gmail.com>
2025-04-16 01:30:28 +0700

    feat: implement AdminLTE layout and templates for welcome page

--- UTS-PWL-POS/routes/web.php
+++ UTS-PWL-POS/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\StokController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\SupplierController;
 
@@ -19,9 +20,8 @@ use App\Http\Controllers\SupplierController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index']);
+
 Route::get('/barang', [BarangController::class, 'index'])->name('barang.index');
 Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
 Route::get('/level', [LevelController::class, 'index'])->name('level.index');
