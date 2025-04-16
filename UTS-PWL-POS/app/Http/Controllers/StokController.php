7cff616928596cecdecc8c62195a1603246e73bf
koctaa04 <yeftaocta04@gmail.com>
2025-04-16 19:20:26 +0700

    feat(stock): Implement create form and Ajax store functionality

--- UTS-PWL-POS/app/Http/Controllers/StokController.php
+++ UTS-PWL-POS/app/Http/Controllers/StokController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\StokModel;
+use App\Models\UserModel;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
 use App\Models\SupplierModel;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 class StokController extends Controller
 {
@@ -53,6 +55,55 @@ class StokController extends Controller
     {
         $stok = StokModel::with(['barang', 'user', 'supplier'])->find($id);
         return view('user.show_ajax', compact('stok'));
+    }
+
+
+    public function create_ajax()
+    {
+        $barang = BarangModel::select('barang_id', 'barang_nama')->get();
+        // $user = UserModel::select('user_id', 'nama')->get();
+        $supplier = SupplierModel::select('supplier_id', 'supplier_nama')->get();
+
+        return view('stok.create_ajax', [
+            'barang' => $barang,
+            // 'user' => $user,
+            'supplier' => $supplier
+        ]);
+    }
+
+    public function store_ajax(Request $request)
+    {
+        // cek apakah request berupa ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'barang_id'    => 'required|integer|exists:m_barang,barang_id',
+                'supplier_id'  => 'required|integer|exists:m_supplier,supplier_id',
+                'stok_tanggal' => 'required|date',
+                'stok_jumlah'  => 'required|integer|min:1',
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false, // response status, false: error/gagal, true: berhasil
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors() // pesan error validasi
+                ]);
+            }
+
+            $data = $request->all();
+            $data['user_id'] = 1;
+            // $data['user_id'] = auth()->user()->user_id;
+            StokModel::create($data);
+
+            return response()->json([
+                'status'  => true,
+                'message' => 'Data stok berhasil disimpan.',
+            ]);
+        }
         
+
+        return redirect('/');
     }
 }
--- UTS-PWL-POS/resources/views/stok/index.blade.php
+++ UTS-PWL-POS/resources/views/stok/index.blade.php
@@ -1,143 +1,143 @@
-@extends('layouts.template')
-@section('content')
-    <div class="card card-outline card-primary">
-        <div class="card-header">
-            <h3 class="card-title">{{ $page->title }}</h3>
-            <div class="card-tools">
-                <button onclick="modalAction('{{ url('/stok/create_ajax') }}')"
-                    class="btn btn-sm btn-success mt-1 mr-4">Tambah Data</button>
+    @extends('layouts.template')
+    @section('content')
+        <div class="card card-outline card-primary">
+            <div class="card-header">
+                <h3 class="card-title">{{ $page->title }}</h3>
+                <div class="card-tools">
+                    <button onclick="modalAction('{{ url('/stok/create_ajax') }}')"
+                        class="btn btn-sm btn-success mt-1 mr-4">Tambah Data</button>
+                </div>
             </div>
-        </div>
-        <div class="card-body">
-            {{-- Alert untuk Success --}}
-            @if (session('success'))
-                <div class="alert alert-success">{{ session('success') }}</div>
-            @endif
+            <div class="card-body">
+                {{-- Alert untuk Success --}}
+                @if (session('success'))
+                    <div class="alert alert-success">{{ session('success') }}</div>
+                @endif
 
-            {{-- Alert untuk Error --}}
-            @if (session('error'))
-                <div class="alert alert-danger">{{ session('error') }}</div>
-            @endif
+                {{-- Alert untuk Error --}}
+                @if (session('error'))
+                    <div class="alert alert-danger">{{ session('error') }}</div>
+                @endif
 
-            {{-- Filtering --}}
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter:</label>
-                        <div class="col-3">
-                            <select class="form-control" id="barang_id" name="barang_id" required>
-                                <option value="">- Semua Barang-</option>
-                                @foreach ($barang as $item)
-                                    <option value="{{ $item->barang_id }}">{{ $item->barang_nama }}</option>
-                                @endforeach
-                            </select>
-                            <small class="form-text text-muted">Nama Barang</small>
-                        </div>
-                        <div class="col-3">
-                            <select class="form-control" id="supplier_id" name="supplier_id" required>
-                                <option value="">- Semua Supplier-</option>
-                                @foreach ($supplier as $item)
-                                    <option value="{{ $item->supplier_id }}">{{ $item->supplier_nama }}</option>
-                                @endforeach
-                            </select>
-                            <small class="form-text text-muted">Supplier</small>
+                {{-- Filtering --}}
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group row">
+                            <label class="col-1 control-label col-form-label">Filter:</label>
+                            <div class="col-3">
+                                <select class="form-control" id="barang_id" name="barang_id" required>
+                                    <option value="">- Semua Barang-</option>
+                                    @foreach ($barang as $item)
+                                        <option value="{{ $item->barang_id }}">{{ $item->barang_nama }}</option>
+                                    @endforeach
+                                </select>
+                                <small class="form-text text-muted">Nama Barang</small>
+                            </div>
+                            <div class="col-3">
+                                <select class="form-control" id="supplier_id" name="supplier_id" required>
+                                    <option value="">- Semua Supplier-</option>
+                                    @foreach ($supplier as $item)
+                                        <option value="{{ $item->supplier_id }}">{{ $item->supplier_nama }}</option>
+                                    @endforeach
+                                </select>
+                                <small class="form-text text-muted">Supplier</small>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+                <table class="table table-bordered table-striped table-hover table-sm" id="table_stok">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Kode Barang</th>
+                            <th>Nama Barang</th>
+                            <th>Supplier</th>
+                            <th>Jumlah</th>
+                            <th>Tanggal</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                </table>
             </div>
-
-            <table class="table table-bordered table-striped table-hover table-sm" id="table_stok">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Kode Barang</th>
-                        <th>Nama Barang</th>
-                        <th>Supplier</th>
-                        <th>Jumlah</th>
-                        <th>Tanggal</th>
-                        <th>Aksi</th>
-                    </tr>
-                </thead>
-            </table>
         </div>
-    </div>
-@endsection
-@push('css')
-@endpush
-@push('js')
-    <script>
-        function modalAction(url = '') {
-            $('#myModal').load(url, function() {
-                $('#myModal').modal('show');
+    @endsection
+    @push('css')
+    @endpush
+    @push('js')
+        <script>
+            function modalAction(url = '') {
+                $('#myModal').load(url, function() {
+                    $('#myModal').modal('show');
+                });
+            }
+
+            var dataStok; // Dideklarasikan di luar agar bisa diakses di semua fungsi
+
+            $(document).ready(function() {
+                dataStok = $('#table_stok').DataTable({
+                    // serverSide: true, jika ingin menggunakan server side processing
+                    serverSide: true,
+                    ajax: {
+                        "url": "{{ url('stok/list') }}",
+                        "dataType": "json",
+                        "type": "POST",
+                        "data": function(data) {
+                            data.barang_id = $('#barang_id').val();
+                            data.supplier_id = $('#supplier_id').val();
+                        }
+                    },
+                    columns: [{
+                            data: "DT_RowIndex",
+                            className: "text-center",
+                            orderable: false,
+                            searchable: false
+                        },
+                        {
+                            data: "barang.barang_kode",
+                            className: "",
+                            orderable: true,
+                            searchable: true
+                        },
+                        {
+                            data: "barang.barang_nama",
+                            className: "",
+                            orderable: true,
+                            searchable: true
+                        },
+                        {
+                            data: "supplier.supplier_nama",
+                            className: "",
+                            orderable: true,
+                            searchable: true
+                        },
+                        {
+                            data: "stok_jumlah",
+                            className: "text-center",
+                            orderable: true,
+                            searchable: false
+                        },
+                        {
+                            data: "stok_tanggal",
+                            className: "text-center",
+                            orderable: true,
+                            searchable: false
+                        },
+                        {
+                            data: "aksi",
+                            className: "text-center",
+                            orderable: false,
+                            searchable: false
+                        }
+                    ]
+                });
+
+
+                $('#barang_id, #supplier_id').on('change', function() {
+                    datastok.ajax.reload();
+                });
             });
-        }
-
-        var datastok; // Dideklarasikan di luar agar bisa diakses di semua fungsi
-
-        $(document).ready(function() {
-            datastok = $('#table_stok').DataTable({
-                // serverSide: true, jika ingin menggunakan server side processing
-                serverSide: true,
-                ajax: {
-                    "url": "{{ url('stok/list') }}",
-                    "dataType": "json",
-                    "type": "POST",
-                    "data": function(data) {
-                        data.barang_id = $('#barang_id').val();
-                        data.supplier_id = $('#supplier_id').val();
-                    }
-                },
-                columns: [{
-                        data: "DT_RowIndex",
-                        className: "text-center",
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: "barang.barang_kode",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "barang.barang_nama",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "supplier.supplier_nama",
-                        className: "",
-                        orderable: true,
-                        searchable: true
-                    },
-                    {
-                        data: "stok_jumlah",
-                        className: "text-center",
-                        orderable: true,
-                        searchable: false
-                    },
-                    {
-                        data: "stok_tanggal",
-                        className: "text-center",
-                        orderable: true,
-                        searchable: false
-                    },
-                    {
-                        data: "aksi",
-                        className: "text-center",
-                        orderable: false,
-                        searchable: false
-                    }
-                ]
-            });
-
-
-            $('#barang_id, #supplier_id').on('change', function() {
-                datastok.ajax.reload();
-            });
-        });
-    </script>
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
-        data-keyboard="false" data-width="75%" aria-hidden="true"></div>
-@endpush
+        </script>
+        <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
+            data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+    @endpush
