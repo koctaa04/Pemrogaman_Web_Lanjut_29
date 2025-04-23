46aa7857e120387aca42388ac2aa7d7aba8af7e4
koctaa04 <yeftaocta04@gmail.com>
2025-04-23 17:24:50 +0700

    JS 11 : First Set Up

---
+++ Pertemuan 11/PWL-POS/app/Models/UserModel.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\LevelModel;
+use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+class UserModel extends Authenticatable implements JWTSubject
+{
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    use HasFactory;
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+        'profile_photo'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+    protected $casts = [
+        'password' => 'hashed'
+    ];
+
+    function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    
+    // Mendapatkan nama role
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    // Cek apakah user memiliki role tertentu
+    public function hasRole(string $role): bool
+    {
+        return $this->level->level_kode === $role;
+    }
+
+    public function getRole() {
+        return $this->level->level_kode;
+    }
+
+}
